3e2f37bda13b15c406f4bd67570a0672539a7033
constantin <mitides.constantin@gmail.com>
2015-08-19 17:52:53 -0400

    Small edits made

--- Assets/_Scripts/LoginManager.cs
+++ Assets/_Scripts/LoginManager.cs
@@ -3,24 +3,24 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class LoginManager : MonoBehaviour {
-	public Text username;
-	public Text gold;
+	// public Text username;
+	// public Text gold;
 
-	// Use this for initialization
-	void Start () {
+	// // Use this for initialization
+	// void Start () {
 
 	
-	}
+	// }
 	
-	// Update is called once per frame
-	void Update () {
-		if (username == 'Admin'){
-			gold.gold = '1000';
+	// // Update is called once per frame
+	// void Update () {
+	// 	if (username == 'Admin'){
+	// 		gold.gold = '1000';
 
 
-			}else{
-				gold.gold = '500';
-			}
+	// 		}else{
+	// 			gold.gold = '500';
+	// 		}
 	
-	}
+	// }
 }
