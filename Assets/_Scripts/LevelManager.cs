6fc33ff272ed9244f034acb903e4a971d12cb121
constantin <mitides.constantin@gmail.com>
2015-08-18 21:25:04 -0400

    This is the first commit it includes two scripts and assets

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level Load: " + name);
+		Application.LoadLevel (name);
+	}
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
