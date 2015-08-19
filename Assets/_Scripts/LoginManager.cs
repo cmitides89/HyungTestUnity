ce15b218a7101b473a764342fe715b93dd199f63
Constantin <mitides.constantin@gmail.com>
2015-08-19 19:40:44 -0400

    added the proper code to get the username

--- Assets/_Scripts/LoginManager.cs
+++ Assets/_Scripts/LoginManager.cs
@@ -3,24 +3,21 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class LoginManager : MonoBehaviour {
-	// public Text username;
-	// public Text gold;
-
-	// // Use this for initialization
-	// void Start () {
-
-	
-	// }
-	
-	// // Update is called once per frame
-	// void Update () {
-	// 	if (username == 'Admin'){
-	// 		gold.gold = '1000';
-
-
-	// 		}else{
-	// 			gold.gold = '500';
-	// 		}
-	
-	// }
+	// click login 
+	//get username - need to find getter function, and then you do username.text
+	//find method: 
+	public string userName;
+	public int gold;
+	private void LoginFunction(){
+		//First you need to find the game object
+		//GameObject userNameInput = GameObject.Find("UserName");
+		//userName = userNameInput.GetComponent<InputField>().text;
+		//better way to write it
+		userName = GameObject.Find ("UserName").GetComponent<InputField>().text;
+		if (userName == "Admin"){
+			gold = 1000;
+		}else{
+			gold = 500;
+		};
+	}
 }
