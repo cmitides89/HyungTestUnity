63d124e59e78f9506e4fe53ce801c33e5216aefe
Constantin <mitides.constantin@gmail.com>
2015-08-19 22:18:00 -0400

    Commit for adding update function its NOT WORKING

--- Assets/_Scripts/LoginManager.cs
+++ Assets/_Scripts/LoginManager.cs
@@ -2,22 +2,31 @@
 using UnityEngine.UI;
 using System.Collections;
 
+
 public class LoginManager : MonoBehaviour {
+	public string userName;
+	public Text totalGold;
+	public Button loginButton;
+
+	void Start (){
+
+	}
+	void Update (){
+		LoginFunction();
+	}
 	// click login 
 	//get username - need to find getter function, and then you do username.text
 	//find method: 
-	public string userName;
-	public int gold;
-	private void LoginFunction(){
+	public void LoginFunction(){
 		//First you need to find the game object
 		//GameObject userNameInput = GameObject.Find("UserName");
 		//userName = userNameInput.GetComponent<InputField>().text;
 		//better way to write it
 		userName = GameObject.Find ("UserName").GetComponent<InputField>().text;
 		if (userName == "Admin"){
-			gold = 1000;
+			totalGold.text = "1000";
 		}else{
-			gold = 500;
+			totalGold.text = "500";
 		};
 	}
 }
