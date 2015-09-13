afdb2222992c1e7c9f3d6839973259df9463d6f2
constantin <mitides.constantin@gmail.com>
2015-09-13 15:42:20 -0400

    minor changes on shop manager

---
+++ Assets/_Scripts/ShopManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour {
+	public string str1;
+	public string str2;
+	public int int1;
+	public int int2;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	// public int RepairFunction(int gold)
+	// {
+		
+	// }
+	// public int SellFunction(int gold)
+	// {
+		
+	// }
+}
